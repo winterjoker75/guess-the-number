1b1bd7ff29c228f4f0f5bd4015e5e8febac6d422
seohyeon <seohyeon@seohyeons-MacBook-Air.local>
2024-10-13 12:14:01 -0700

    feat: number box and container

--- src/scenes/single_play.py
+++ src/scenes/single_play.py
@@ -2,13 +2,13 @@ import sys
 import pygame
 from pygame.locals import *
 from scripts.utils import Text, TextBox
-from scripts.game import SingleGame
+from scripts.game import SingleGame, Container
 
 def single_play(screen, clock):
   game = SingleGame()
 
   attempt = Text("", 40, (0, 0, 0), [320, 100])
-  result = Text("", 40, (0, 0, 0), [320, 200])
+  container = Container([270, 200])
   text_box = TextBox([160, 100], (0, 0, 255), [240, 300], text_size=80)
 
   running = True
@@ -16,11 +16,11 @@ def single_play(screen, clock):
     data = game.receive_data()
 
     attempt.update(str(data["attempt_count"]))
-    result.update(data["result"])
+    container.update(data["result"])
 
     screen.fill((255, 255, 255))
     attempt.draw(screen)
-    result.draw(screen)
+    container.draw(screen)
     text_box.draw(screen)
 
     for event in pygame.event.get():
@@ -29,7 +29,10 @@ def single_play(screen, clock):
         sys.exit()
       elif event.type==pygame.KEYDOWN:
         if event.key==K_RETURN:
-          game.submit_number(text_box.flush_number())
+          flushed_number = text_box.flush_number()
+          if flushed_number and container.left<=flushed_number<=container.right:
+            container.set_next(flushed_number)
+            game.submit_number(flushed_number)
         elif event.key==K_BACKSPACE:
           text_box.erase_number()
         elif event.unicode.isdigit():
